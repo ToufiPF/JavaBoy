b84d203b14fe3501c3681d32ab657a94b1c41043
Unknown <aureli1.clergeot@laposte.net>
2019-06-07 17:31:18 +0200

    LcdLine & Builder

--- src/ch/epfl/javaboy/component/lcd/LcdImageLine.java
+++ src/ch/epfl/javaboy/component/lcd/LcdImageLine.java
@@ -6,10 +6,32 @@ import ch.epfl.javaboy.bits.BitVector;
 import ch.epfl.javaboy.bits.Bits;
 
 public final class LcdImageLine {
+
+    public static final class Builder {
+        private final BitVector.Builder msb, lsb;
+
+        public Builder(int size) {
+            msb = new BitVector.Builder(size);
+            lsb = new BitVector.Builder(size);
+        }
+
+        public void setBytes(int index, byte ms, byte ls) {
+            msb.setByte(index, ms);
+            lsb.setByte(index, ls);
+        }
+        
+        public LcdImageLine build() {
+            BitVector m = msb.build();
+            BitVector l = lsb.build();
+            BitVector opac = m.or(l);
+            return new LcdImageLine(m, l, opac);
+        }
+    }
+
     private final static byte IDENTITY = (byte) 0b11100100;
-    
+
     private final BitVector msb, lsb, opacity;
-    
+
     public LcdImageLine(int size) {
         msb = new BitVector(size);
         lsb = new BitVector(size);
@@ -20,11 +42,11 @@ public final class LcdImageLine {
         this.lsb = lsb;
         this.opacity = opacity;
     }
-    
+
     public int size() {
         return msb.size();
     }
-    
+
     public BitVector msb() {
         return msb;
     }
@@ -34,20 +56,20 @@ public final class LcdImageLine {
     public BitVector opacity() {
         return opacity;
     }
-    
+
     public LcdImageLine shift(int distance) {
         return new LcdImageLine(msb.shift(distance), lsb.shift(distance), opacity.shift(distance));
     }
-    
+
     public LcdImageLine extractWrapped(int start, int size) {
         return new LcdImageLine(msb.extractWrapped(start, size), 
                 lsb.extractWrapped(start, size), opacity.extractWrapped(start, size));
     }
-    
+
     public LcdImageLine mapColors(byte colors) {
         if (colors == IDENTITY)
             return this;
-        
+
         final int LENGTH = 4;
         BitVector[] bitsByColor = {
                 msb.or(lsb).not(),      // 00 
@@ -58,13 +80,19 @@ public final class LcdImageLine {
         int [] colorMap = new int[LENGTH];
         for (int i = 0 ; i < LENGTH ; ++i)
             colorMap[i] = Bits.extract(colors, 2 * i, 2);
-        
-        BitVector m[] = new BitVector[LENGTH];
-        BitVector l[] =  new BitVector[LENGTH];
-        
-        return new LcdImageLine(0);
+
+        final BitVector bits1 = new BitVector(size(), true);
+        final BitVector bits0 = new BitVector(size(), false);
+
+        BitVector m = bitsByColor[0].and(Bits.test(colorMap[0], 1) ? bits1 : bits0);
+        BitVector l = bitsByColor[0].and(Bits.test(colorMap[0], 0) ? bits1 : bits0);
+        for (int i = 1 ; i < LENGTH ; ++i) {
+            m = m.or(bitsByColor[i].and(Bits.test(colorMap[i], 1) ? bits1 : bits0));
+            l = l.or(bitsByColor[i].and(Bits.test(colorMap[i], 0) ? bits1 : bits0));
+        }
+        return new LcdImageLine(m, l, opacity);
     }
-    
+
     public LcdImageLine below(LcdImageLine above) {
         return below(above, above.opacity);
     }
@@ -73,7 +101,7 @@ public final class LcdImageLine {
         BitVector l = opacity.and(above.lsb).or(opacity.not().and(lsb));
         return new LcdImageLine(m, l, this.opacity.or(opacity));
     }
-    
+
     public LcdImageLine join(LcdImageLine other, int start) {
         if (!(0 <= start && start < size()))
             throw new IllegalArgumentException();
@@ -81,7 +109,7 @@ public final class LcdImageLine {
         opacity = opacity.shift(start);
         return below(other, opacity.not());
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof LcdImageLine) {
@@ -93,5 +121,13 @@ public final class LcdImageLine {
     @Override
     public int hashCode() {
         return Objects.hash(msb, lsb, opacity);
+    }
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder(3 * size() + 3);
+        b.append(msb.toString()).append('\n');
+        b.append(lsb.toString()).append('\n');
+        b.append(opacity.toString());
+        return b.toString();
     }
 }
