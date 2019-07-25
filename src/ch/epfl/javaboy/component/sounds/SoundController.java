bf8f79eebae58e2d320e4c845370413b7f5fe76f
Toufi <aureli1.clergeot@laposte.net>
2019-07-26 01:08:01 +0200

    Renaming Speaker -> SoundController

--- src/ch/epfl/javaboy/component/sounds/SoundController.java
+++ src/ch/epfl/javaboy/component/sounds/SoundController.java
@@ -8,11 +8,12 @@ import ch.epfl.javaboy.AddressMap;
 import ch.epfl.javaboy.Preconditions;
 import ch.epfl.javaboy.Register;
 import ch.epfl.javaboy.RegisterFile;
+import ch.epfl.javaboy.bits.Bits;
 import ch.epfl.javaboy.component.Component;
 
-public final class Speaker implements Component {
+public final class SoundController implements Component {
     
-    public static enum NR5 implements Register {
+    public enum NR5 implements Register {
         NR50, NR51, NR52;
         public final static List<NR5> ALL = Collections.unmodifiableList(Arrays.asList(values()));
     }
@@ -27,12 +28,12 @@ public final class Speaker implements Component {
     private final NoiseChannel noise;
     private final RegisterFile<NR5> NR5Regs;
     
-    public Speaker() {
+    public SoundController() {
         square1 = new Square1Channel();
         square2 = new Square2Channel();
         wave = new WaveChannel();
         noise = new NoiseChannel();
-        NR5Regs = new RegisterFile<NR5>(NR5.values());
+        NR5Regs = new RegisterFile<>(NR5.values());
     }
     
     @Override
@@ -55,9 +56,23 @@ public final class Speaker implements Component {
     public void write(int address, int value) {
         Preconditions.checkBits16(address);
         Preconditions.checkBits8(value);
+
+        if (addressInNr5(address)) {
+            NR5 reg = NR5.ALL.get(address - AddressMap.REGS_NR5_START);
+            if (reg == NR5.NR52)
+                NR5Regs.set(NR5.NR52, (0xF0 & value) | (0xF & NR5Regs.get(NR5.NR52)));
+            else
+                NR5Regs.set(reg, value);
+            return;
+        }
+
         square1.write(address, value);
         square2.write(address, value);
         wave.write(address, value);
         noise.write(address, value);
+    }
+
+    private boolean soundCountrollerEnabled() {
+        return Bits.test(NR5Regs.get(NR5.NR52), Sound.NR52Bits.POWER_CONTROL_BIT);
     }
 }
