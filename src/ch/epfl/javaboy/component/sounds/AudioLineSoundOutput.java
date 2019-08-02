561791895068afa342ec6896dcd5a03a4547d0cf
Toufi <aureli1.clergeot@laposte.net>
2019-08-02 22:20:22 +0200

    Sound To Thread

--- src/ch/epfl/javaboy/component/sounds/AudioLineSoundOutput.java
+++ src/ch/epfl/javaboy/component/sounds/AudioLineSoundOutput.java
@@ -9,45 +9,72 @@ public class AudioLineSoundOutput implements SoundOutput {
     public static final int SAMPLE_RATE = 44100;
     private static final int SAMPLES_PER_FRAME = 2;
     private static final AudioFormat FORMAT = new AudioFormat(SAMPLE_RATE, Byte.SIZE, SAMPLES_PER_FRAME, true, false);
-
     private static final int LINE_BUFFER_SIZE = 11025;
+
     private static final int BUFFER_SIZE = 2048;
+    private static final int BUFFER_COUNT = 2;
 
     private SourceDataLine line;
+    private boolean playing;
 
-    private byte[] buffer;
-    private int index;
+    private byte[][] buffers;
+    private int[] indexes;
+    int sel;
+
+    private Thread playback;
 
     public AudioLineSoundOutput() throws LineUnavailableException {
         line = AudioSystem.getSourceDataLine(FORMAT);
         line.open(FORMAT, LINE_BUFFER_SIZE);
 
-        buffer = new byte[BUFFER_SIZE];
+        playback = new Thread(() -> {
+            while (playing) {
+                emptyBuffer();
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+        playback.setDaemon(true);
     }
 
     @Override
     public void start() {
         line.start();
+        playing = true;
+        playback.start();
 
-        index = 0;
+
+        buffers = new byte[BUFFER_COUNT][BUFFER_SIZE];
+        indexes = new int[] { 0, 0 };
+        sel = 0;
     }
 
     @Override
     public void stop() {
+        playing = false;
+
         line.drain();
         line.stop();
     }
 
     @Override
     public void play(int left, int right) {
-        buffer[index++] = (byte) left;
-        buffer[index++] = (byte) right;
-
-        if (index >= BUFFER_SIZE) {
-            int samples = Math.min(BUFFER_SIZE, line.available());
-            line.write(buffer, 0, samples);
-
-            index = 0;
+        if (!playing || indexes[sel] >= BUFFER_SIZE) {
+            //System.err.println("Buffer Overflow !");
+            return;
         }
+        buffers[sel][indexes[sel]++] = (byte) left;
+        buffers[sel][indexes[sel]++] = (byte) right;
+    }
+
+    private void emptyBuffer() {
+        int prev = sel;
+        ++sel;
+        if (sel == BUFFER_COUNT)
+            sel = 0;
+        line.write(buffers[prev], 0, indexes[prev]);
+        indexes[prev] = 0;
     }
 }
